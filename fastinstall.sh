c763e43f14b5cfbc9e8c611e3bdf2900a13542f8
dxomg <101947415+dxomg@users.noreply.github.com>
2022-06-02 16:24:03 -0600

    Auto Download Proxies

--- fastinstall.sh
+++ fastinstall.sh
@@ -1,3 +1,6 @@
 git clone https://github.com/dxomg/mcbot
 cd mcbot
+rm proxies.txt
+wget https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks4.txt
+mv socks4.txt proxies.txt
 java -jar MCBOT.jar libertyversions.aternos.me:39400 47 botjoiner 300 40000
