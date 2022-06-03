1ba5e9b4d7bccc809e1a8cb226c95e2ac3c635b4
dxomg <101947415+dxomg@users.noreply.github.com>
2022-06-03 17:43:32 -0600

    Update fastinstall.sh

--- fastinstall.sh
+++ fastinstall.sh
@@ -3,4 +3,4 @@ cd mcbot
 rm proxies.txt
 wget https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks4.txt
 mv socks4.txt proxies.txt
-java -jar MCBOT.jar libertyversions.aternos.me:39400 47 botjoiner 300 40000
+java -jar MCBOT.jar de8.plutonodes.net:25344 47 botjoiner 300 40000
