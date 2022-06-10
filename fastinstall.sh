84a046ca6d5f12d0289cd4494d084a10dfb03bd7
dxomg <101947415+dxomg@users.noreply.github.com>
2022-06-09 21:24:24 -0600

    Update fastinstall.sh

--- fastinstall.sh
+++ fastinstall.sh
@@ -3,4 +3,3 @@ cd mcbot
 rm proxies.txt
 wget https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks4.txt
 mv socks4.txt proxies.txt
-java -jar MCBOT.jar Zinsentidoo.aternos.me:28340 47 longhost 300 40000
