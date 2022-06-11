0eab4bd8f1849f668989f8c43241e416a6b26f8b
dxomg <101947415+dxomg@users.noreply.github.com>
2022-06-11 14:50:41 -0600

    Update fastinstall.sh

--- fastinstall.sh
+++ fastinstall.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/dxomg/stuff
-cd mcbot
+cd stuff
 rm proxies.txt
 wget https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks4.txt
 mv socks4.txt proxies.txt
